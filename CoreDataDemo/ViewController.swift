5f76a9906021864f2d9e9a7a639c92e673e24dc7
sikandar <sikandar.ali307@gmail.com>
2021-05-20 14:50:30 +0500

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Sikandar Ali on 20/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
